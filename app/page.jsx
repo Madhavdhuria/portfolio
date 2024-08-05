588a9dbd83bcfe44d0aed8d2b7e3902198623090
Madhavdhuria <madhavdhuria556@gmail.com>
2024-08-06 02:23:42 +0530

    Services page done

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,18 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Socials from "../components/Socials";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 export default function Home() {
   return (
     <section className="h-full">
       <div className="mx-auto container">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br />
-              <span className="text-accent">Madhav</span>
+              <span className="text-accent">Madhav dhuria</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegent digital expiriences and I am
@@ -34,9 +36,12 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 }
