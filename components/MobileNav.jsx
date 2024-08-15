5f6e55285e38cca67f1d4d79ac5ef3e9c89ca24b
Madhavdhuria <madhavdhuria556@gmail.com>
2024-08-15 13:56:01 +0530

    fixing mobile nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,15 +3,12 @@ import Link from "next/link";
 import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 const links = [
   {
     name: "home",
     path: "/",
-  },
-  {
-    name: "services",
-    path: "/services",
   },
   {
     name: "resume",
@@ -29,10 +26,18 @@ const links = [
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        className="flex justify-center items-center"
+        onClick={() => setIsOpen(true)}
+      >
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
@@ -45,20 +50,18 @@ const MobileNav = () => {
           </Link>
         </div>
         <nav className="flex flex-col gap-8 justify-center items-center">
-          {links.map((link, index) => {
-            return (
-              <Link
-                href={link.path}
-                key={index}
-                className={`text-xl capitalize hover:text-accent transition-all ${
-                  pathname === link.path &&
-                  "text-accent border-b-2 border-accent"
-                }`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              className={`text-xl capitalize hover:text-accent transition-all ${
+                pathname === link.path && "text-accent border-b-2 border-accent"
+              }`}
+              onClick={handleLinkClick} 
+            >
+              {link.name}
+            </Link>
+          ))}
         </nav>
       </SheetContent>
     </Sheet>
